f5ea467ca360cfaeb2f3b62a616cc6660d976407
MaxA07 <amorfis77@gmail.com>
2022-09-11 21:27:16 +0300

    added map on activity

---
+++ app/src/main/java/com/example/appwithymaps/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.appwithymaps
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.GeoObjectTapEvent
+import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.layers.GeoObjectTapListener
+import com.yandex.mapkit.map.*
+import com.yandex.mapkit.map.Map
+
+class MainActivity : AppCompatActivity(), GeoObjectTapListener, InputListener {
+
+    lateinit var mapView: MapView
+
+    lateinit var mapObjects: MapObjectCollection
+    lateinit var animationHandler: Handler
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey("2bdc136b-2fd9-453d-ad00-1d3efa4b23e0")
+        MapKitFactory.initialize(this)
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView.map.move(
+            CameraPosition(Point(56.305326, 43.996373), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 2f), null
+        )
+        mapObjects = mapView.map.mapObjects.addCollection()
+        animationHandler = Handler(Looper.getMainLooper())
+
+        mapView.map.addTapListener(this)
+        mapView.map.addInputListener(this)
+
+
+    }
+
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+        super.onStart()
+    }
+    override fun onObjectTap(geoObjectTapEvent: GeoObjectTapEvent): Boolean {
+        val selectionMetadata = geoObjectTapEvent
+            .geoObject
+            .metadataContainer
+            .getItem(GeoObjectSelectionMetadata::class.java)
+        if (selectionMetadata != null) {
+            mapView.map.selectGeoObject(selectionMetadata.id, selectionMetadata.layerId)
+        }
+        return selectionMetadata != null
+    }
+
+    override fun onMapTap(map: Map, point: Point) {
+        mapView.map.deselectGeoObject()
+    }
+
+    override fun onMapLongTap(map: Map, point: Point) {}
+
+
+}
+
