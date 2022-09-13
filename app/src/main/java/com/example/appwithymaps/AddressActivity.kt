0c048544ec53d9f1c7df51e0bc1433b8f3a6245f
MaxA07 <amorfis77@gmail.com>
2022-09-13 16:53:13 +0300

    added activity with address

---
+++ app/src/main/java/com/example/appwithymaps/AddressActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appwithymaps
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        
+    }
+}
