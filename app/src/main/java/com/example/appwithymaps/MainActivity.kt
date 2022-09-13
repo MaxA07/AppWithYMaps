9d675997e1bf2fd619ef6f4323df89f69c1289cc
MaxA07 <amorfis77@gmail.com>
2022-09-13 14:41:21 +0300

    added first screen

--- app/src/main/java/com/example/appwithymaps/MainActivity.kt
+++ app/src/main/java/com/example/appwithymaps/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.appwithymaps
 
+import android.location.Geocoder
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.yandex.mapkit.Animation
+import com.yandex.mapkit.GeoObject
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Geo
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.layers.GeoObjectTapEvent
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.mapkit.layers.GeoObjectTapListener
+
 import com.yandex.mapkit.map.*
 import com.yandex.mapkit.map.Map
+import com.yandex.runtime.image.ImageProvider
 
 class MainActivity : AppCompatActivity(), GeoObjectTapListener, InputListener {
 
@@ -38,9 +43,7 @@ class MainActivity : AppCompatActivity(), GeoObjectTapListener, InputListener {
         mapView.map.addTapListener(this)
         mapView.map.addInputListener(this)
 
-
     }
-
 
     override fun onStop() {
         mapView.onStop()
@@ -53,6 +56,7 @@ class MainActivity : AppCompatActivity(), GeoObjectTapListener, InputListener {
         MapKitFactory.getInstance().onStart()
         super.onStart()
     }
+
     override fun onObjectTap(geoObjectTapEvent: GeoObjectTapEvent): Boolean {
         val selectionMetadata = geoObjectTapEvent
             .geoObject
@@ -66,10 +70,21 @@ class MainActivity : AppCompatActivity(), GeoObjectTapListener, InputListener {
 
     override fun onMapTap(map: Map, point: Point) {
         mapView.map.deselectGeoObject()
+
+        val mapsObject = mapView.map.mapObjects
+        //.setText(point.latitude.toString() + " " + point.longitude.toString())
+        val geo = Geocoder(this)
+        val inf = geo.getFromLocation(point.latitude, point.longitude, 1)
+        val address = inf[0].getAddressLine(0)
+        val pin = mapsObject.addPlacemark(point).setText(address + "" + "\n" + "(${point.latitude} ${point.longitude})" )
+
     }
 
-    override fun onMapLongTap(map: Map, point: Point) {}
 
+
+    override fun onMapLongTap(map: Map, point: Point) {
+
+    }
 
 }
 
